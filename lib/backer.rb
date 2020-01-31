e1db54028767dac6915ab6d9e2f0186912f62f38
anamsoomro <anamsoomroed@gmail.com>
2020-01-31 15:34:59 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+class Backer
+    attr_reader :name, :backed_projects
+
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        @backed_projects = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def back_project(project)
+        @backed_projects << project
+        project.backers << self ## !single_source_of_truth
+    end
+
+end
